3588ca4f52fb8e3feca1062a6f8d34706e079767
Ferariu-Adina <ferariuadina@yahoo.com>
2024-02-16 16:06:13 +0200

    CaesarShiftCipher test

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,37 @@
 package tudelft.caesarshift;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class CaesarShiftCipherTest {
+	
+	@Test
+	public void stringAndPositiveShift() {
+		CaesarShiftCipher chiper = new CaesarShiftCipher();
+		String result = chiper.caesarShiftCipher("abc", 3);
+		assertEquals("def", result);
+	}
+	
+	@Test
+	public void stringAndNegativeShift() {
+		CaesarShiftCipher chiper = new CaesarShiftCipher();
+		String result = chiper.caesarShiftCipher("abc", -3);
+		assertEquals("xyz", result);
+	}
+	
+	@Test
+	public void endStringAndPositiveShift() {
+		CaesarShiftCipher chiper = new CaesarShiftCipher();
+		String result = chiper.caesarShiftCipher("xyz", 3);
+		assertEquals("abc", result);
+	}
+	
+	@Test
+	public void endStringAndNegativeShift() {
+		CaesarShiftCipher chiper = new CaesarShiftCipher();
+		String result = chiper.caesarShiftCipher("xyz", -3);
+		assertEquals("uvw", result);
+	}
 
 }
