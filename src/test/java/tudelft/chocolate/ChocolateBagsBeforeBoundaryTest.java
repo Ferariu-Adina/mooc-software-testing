6fff4b2c6a1d793b90b68307dae3f28920e84465
Ferariu-Adina <ferariuadina@yahoo.com>
2024-02-16 12:25:10 +0200

    GHappy test

--- src/test/java/tudelft/chocolate/ChocolateBagsBeforeBoundaryTest.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsBeforeBoundaryTest.java
@@ -4,28 +4,32 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ChocolateBagsBeforeBoundaryTest {
-    @Test
-    public void totalIsBiggerThanAmountOfBars() {
-        ChocolateBags bags = new ChocolateBags();
-        int result = bags.calculate(1, 1, 10);
-        Assertions.assertEquals(-1, result);
-    }
-
-    @Test
-    public void onlyBigBars() {
-        int result = new ChocolateBags().calculate(5, 3, 10);
-        Assertions.assertEquals(0, result);
-    }
-
-    @Test
-    public void bigAndSmallBars() {
-        int result = new ChocolateBags().calculate(5, 3, 17);
-        Assertions.assertEquals(2, result);
-    }
-
-    @Test
-    public void onlySmallBars() {
-        int result = new ChocolateBags().calculate(4, 2, 3);
-        Assertions.assertEquals(3, result);
-    }
+   @Test
+   public void totalIsTooBig() {
+	   ChocolateBags bags = new ChocolateBags();
+	   int result = bags.calculate(1, 1, 10);
+	   Assertions.assertEquals(-1, result);
+   }
+   
+   @Test
+   public void onlyBigBars() {
+	   ChocolateBags bags = new ChocolateBags();
+	   int result = bags.calculate(3, 3, 10);
+	   Assertions.assertEquals(0, result);
+   }
+   
+   @Test
+   public void smallAndBigBars() {
+	   ChocolateBags bags = new ChocolateBags();
+	   int result = bags.calculate(2, 3, 17);
+	   Assertions.assertEquals(2, result);
+   }
+   
+   @Test
+   public void onlySmallBars() {
+	   ChocolateBags bags = new ChocolateBags();
+	   int result = bags.calculate(3, 3, 3);
+	   Assertions.assertEquals(3, result);
+   }
+   
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,35 @@
 package tudelft.ghappy;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+	
+	@Test
+	public void onlyOneG() {
+		GHappy ghappy = new GHappy();
+		boolean result = ghappy.gHappy("xxgxx");
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void twoG() {
+		GHappy ghappy = new GHappy();
+		boolean result = ghappy.gHappy("xxggxx");
+		assertEquals(true, result);
+	}
+	
+	@Test
+	public void oneHappyGandoneNot() {
+		GHappy ghappy = new GHappy();
+		boolean result = ghappy.gHappy("xxggyygxx");
+		assertEquals(false, result);
+	}
+	@Test
+	public void twoHappyG() {
+		GHappy ghappy = new GHappy();
+		boolean result = ghappy.gHappy("xxggxxgg");
+		assertEquals(true, result);
+	}
 }
