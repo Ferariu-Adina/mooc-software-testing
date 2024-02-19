d80e59efea85f1f830b4de00e8f3edc2f37cffd9
Ferariu-Adina <ferariuadina@yahoo.com>
2024-02-19 10:21:24 +0200

    Count letters test

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -16,5 +16,34 @@ public class CountLettersTest {
         int words = new CountLetters().count("cats|dog");
         Assertions.assertEquals(1, words);
     }
+    
+    @Test
+    public void oneSMatchingWord() {
+        int words = new CountLetters().count("cats");
+        Assertions.assertEquals(1, words);
+    }
+    @Test
+    public void oneRMatchingWord() {
+        int words = new CountLetters().count("car");
+        Assertions.assertEquals(1, words);
+    }
+    
+    @Test
+    public void oneRAndOneSMatchingWords() {
+        int words = new CountLetters().count("car|dogs");
+        Assertions.assertEquals(2, words);
+    }
+    @Test
+    public void oneRMatchingWordaAndOnenotMatching() {
+        int words = new CountLetters().count("cats");
+        Assertions.assertEquals(1, words);
+    }
+    
+    @Test
+    public void noMatchingWord() {
+        int words = new CountLetters().count("cat|dog");
+        Assertions.assertEquals(0, words);
+    }
+    
 
 }
